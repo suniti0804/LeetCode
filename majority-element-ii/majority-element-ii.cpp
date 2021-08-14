3e4fabacd7de4ce44cd3028b58e7f9534e1c3916
Suniti <72435330+suniti0804@users.noreply.github.com>
2021-08-14 11:23:47 +0530

    Time: 8 ms (94.70%), Space: 16 MB (9.48%) - LeetHub

---
+++ majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) 
+    {
+        //max two elements can be greater than n/3
+        int n = nums.size();
+        int count1 = 0, count2 = 0;
+        int first = INT_MIN, second = INT_MIN;
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] == first)
+                count1++;
+            else if(nums[i] == second)
+                count2++;
+            else if(count1 == 0)
+            {
+                first = nums[i];
+                count1++;
+            }
+            else if(count2 == 0)
+            {
+                second = nums[i];
+                count2++;
+            }
+            
+            else
+            {
+                count1--;
+                count2--;
+            }
+        }
+        
+        count1 = count2 = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] == first)
+                count1++;
+            else if(nums[i] == second)
+                count2++;
+        }
+        
+        vector<int> res;
+        if(count1 > n/3)
+            res.push_back(first);
+        if(count2 > n/3)
+            res.push_back(second);
+        
+        return res;
+    }
+};
