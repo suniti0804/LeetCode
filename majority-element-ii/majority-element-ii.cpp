ff257e7e938a25a3778ad3f33220f4614d57ec7d
Suniti <72435330+suniti0804@users.noreply.github.com>
2021-09-23 08:36:10 +0530

    Time: 16 ms (40.80%), Space: 15.8 MB (48.90%) - LeetHub

--- majority-element-ii/majority-element-ii.cpp
+++ majority-element-ii/majority-element-ii.cpp
@@ -16,12 +16,12 @@ public:
             else if(count1 == 0)
             {
                 first = nums[i];
-                count1++;
+                count1 = 1;
             }
             else if(count2 == 0)
             {
                 second = nums[i];
-                count2++;
+                count2 = 1;
             }
             
             else
