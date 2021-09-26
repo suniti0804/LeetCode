4400c03f5d8720e7ac593730f1f3076300382ac9
Suniti <72435330+suniti0804@users.noreply.github.com>
2021-09-26 10:48:45 +0530

    Time: 4 ms (80.29%), Space: 9.5 MB (90.62%) - LeetHub

--- search-insert-position/search-insert-position.cpp
+++ search-insert-position/search-insert-position.cpp
@@ -8,16 +8,15 @@ public:
         
         while(s <= e)
         {
-            int mid = s + (e - s)/2;
-            if(nums[mid] == target)
+            int mid = s+(e-s)/2;
+            if(nums[mid]==target)
                 return mid;
-            if(nums[mid] > target)
-                e = mid - 1;
+            else if(nums[mid]>target)
+                e=mid-1;
             else
-                s = mid + 1;
-
+                s=mid+1;
         }
-
+        
         return s;
     }
 };
