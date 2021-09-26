9b4b117efdf1d284ba2b04262047e680ffdc0de8
Suniti <72435330+suniti0804@users.noreply.github.com>
2021-09-26 15:52:57 +0530

    Time: 20 ms (63.15%), Space: 23.4 MB (54.95%) - LeetHub

---
+++ max-area-of-island/max-area-of-island.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    //refer to number of islands
+    void DFS(vector<vector<int>>& grid, int i, int j, int& a)
+    {
+        // boundary checking
+        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size())
+            return;
+        if(grid[i][j] == 0)
+            return;
+        // return if current position is of water or is already visited
+        a++;
+        // mark the current as visited
+        grid[i][j] = 0;
+          
+        
+        // do DFS in all 4 directions
+        DFS(grid, i+1, j, a);
+        DFS(grid, i, j-1, a);
+        DFS(grid, i-1, j, a);
+        DFS(grid, i, j+1, a);
+        
+    }
+
+    
+    int maxAreaOfIsland(vector<vector<int>>& grid) 
+    {
+        int ans = 0;
+        
+        for(int i = 0; i < grid.size(); i++) 
+        {
+            for(int j = 0; j < grid[0].size(); j++)
+            {
+                // do DFS in case has not been visited and there is land
+                if(grid[i][j] == 1) 
+                {
+                    int a = 0;
+                    DFS(grid, i, j, a);
+                    ans = max(ans, a);
+                }
+            }
+        }
+        return ans;
+    }
+};
