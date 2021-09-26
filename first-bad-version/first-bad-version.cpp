3cd528fd47c420f13bb00d84de7c2c4c0aec7cfb
Suniti <72435330+suniti0804@users.noreply.github.com>
2021-09-26 10:27:08 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (67.26%) - LeetHub

--- first-bad-version/first-bad-version.cpp
+++ first-bad-version/first-bad-version.cpp
@@ -8,16 +8,14 @@ public:
         int s = 1;
         int e = n;
         
-        while(s < e)
+        while(s <= e)
         {
-            int mid = s + (e - s) / 2;
-            
+            int mid = s+(e-s)/2;
             if(isBadVersion(mid))
-                e = mid;
+                e=mid-1;
             else
-                s = mid + 1;
+                s=mid+1;
         }
-        
         return s;
     }
 };
