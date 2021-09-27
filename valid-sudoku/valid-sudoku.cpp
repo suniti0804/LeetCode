c0948d7d71b292878128c7166eb5f646d6611431
Suniti <72435330+suniti0804@users.noreply.github.com>
2021-09-27 20:26:11 +0530

    Time: 20 ms (75.98%), Space: 18 MB (71.94%) - LeetHub

--- valid-sudoku/valid-sudoku.cpp
+++ valid-sudoku/valid-sudoku.cpp
@@ -35,19 +35,27 @@ public:
     
     bool isValidSudoku(vector<vector<char>> &board)
     {
-        int row[9][9] = {0}, col[9][9] = {0}, box[9][9] = {0};
-        for(int i = 0; i < 9; i++)
+        int row[9][9]={0};
+        int col[9][9]={0};
+        int box[9][9]={0};
+        
+        for(int i=0;i<9;i++)
         {
-            for(int j = 0; j < 9; j++)
-                if(board[i][j] != '.')
+            for(int j=0; j<9; j++)
+            {
+                if(board[i][j]!='.')
                 {
-                    int num = board[i][j] - '0' - 1;
-                    int k = i / 3 * 3 + j / 3;
-                    //check then increment
-                    if(row[i][num]++ || col[j][num]++ || box[k][num]++)
+                    int num=board[i][j]-'0'-1;
+                    if(row[i][num]++>0)
+                        return false;
+                    if(col[j][num]++>0)
+                        return false;
+                    if(box[i/3*3+j/3][num]++>0)
                         return false;
                 }
-        } 
+            }
+        }
+        
         return true;
     }
 };
