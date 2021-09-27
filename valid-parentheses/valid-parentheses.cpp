dfc82676a32f5685cb4a82c630ff728266f93aad
Suniti <72435330+suniti0804@users.noreply.github.com>
2021-09-27 11:45:07 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (79.96%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+       if(s[0]==')'||s[0]==']'||s[0]=='}')
+           return false;
+        stack<char> st;
+        int i=0;
+        while(i<s.size())
+        {
+            if(s[i]=='('||s[i]=='['||s[i]=='{'||st.empty())
+                st.push(s[i]);
+            else
+            {
+                if(s[i]==')'&&st.top()!='(')
+                    return false;
+                if(s[i]==']'&&st.top()!='[')
+                    return false;
+                if(s[i]=='}'&&st.top()!='{')
+                    return false;
+                st.pop();
+            }
+            i++;
+        }
+        return st.empty();
+    }
+};
