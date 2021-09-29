a95451bee34a2e71850ad0c2f25cf7753794d6e7
Suniti <72435330+suniti0804@users.noreply.github.com>
2021-09-29 18:36:23 +0530

    Time: 80 ms (94.30%), Space: 96.1 MB (85.40%) - LeetHub

---
+++ island-perimeter/island-perimeter.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    
+        int numofneighbour(vector<vector<int>>&mat, int i, int j, int R, int C)
+        {
+            int count = 0;
+
+            // UP
+            if (i > 0 && mat[i - 1][j])
+                count++;
+
+            // LEFT
+            if (j > 0 && mat[i][j - 1])
+                count++;
+
+            // DOWN
+            if (i < R-1 && mat[i + 1][j])
+                count++;
+
+            // RIGHT
+            if (j < C-1 && mat[i][j + 1])
+                count++;
+
+            return count;
+        }
+ 
+    
+    int islandPerimeter(vector<vector<int>>& mat) 
+    {
+        int perimeter = 0;
+        int R=mat.size();
+        int C=mat[0].size();
+ 
+        // Traversing the matrix and finding ones to
+        // calculate their contribution.
+        for (int i = 0; i < R; i++)
+            for (int j = 0; j < C; j++)
+                if (mat[i][j])
+                    perimeter += (4 - numofneighbour(mat, i ,j, R, C));
+
+        return perimeter;
+
+    }
+};
