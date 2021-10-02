a9111b8f43974b4660724f900f97ee738307e596
Suniti <72435330+suniti0804@users.noreply.github.com>
2021-10-02 15:57:34 +0530

    Time: 45 ms (5.12%), Space: 13.1 MB (82.70%) - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.cpp
+++ set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -11,12 +11,15 @@ public:
             {
                 if(matrix[i][j] == 0) 
                 {
-                    if(i == 0) row = true;
-                    if(j == 0) col = true;
+                    if(i == 0) 
+                        row = true;
+                    if(j == 0) 
+                        col = true;
                     matrix[0][j] = matrix[i][0] = 0;
                 }
             }
         }
+        
         for(int i = 1; i < m; i++)
         {
             for(int j = 1; j < n; j++)
