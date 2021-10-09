a9df74d9456854cb2c1fc1f1bf7186777d12e1d5
Suniti <72435330+suniti0804@users.noreply.github.com>
2021-10-09 20:22:23 +0530

    Time: 47 ms (32.88%), Space: 36.9 MB (71.22%) - LeetHub

---
+++ two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   bool findTarget(TreeNode* root, int target) 
+   {
+    
+   if(!root) return false;
+
+    stack<TreeNode*> s1, s2;
+    TreeNode* temp1 = root;
+    TreeNode* temp2 = root;
+
+
+    while(1)
+    {
+        while(temp1)
+        {
+            s1.push(temp1);
+            temp1 = temp1->left;
+        }
+
+        while(temp2)
+        {
+            s2.push(temp2);
+            temp2 = temp2->right;
+        }
+
+        if(s1.empty() || s2.empty()) break;
+
+        temp1 = s1.top();
+        temp2 = s2.top();
+
+        if((temp1->val + temp2->val) == target)
+        {
+            if(temp1 == temp2) return false;
+
+            return true;
+        }
+
+        else if((temp1->val + temp2->val) < target)
+        {
+            s1.pop();
+            temp1 = temp1->right;
+            temp2 = NULL;
+        }
+        else
+        {
+            s2.pop();
+            temp2 = temp2->left;
+            temp1 = NULL;
+        }
+    }
+
+    return false;
+}
+};
