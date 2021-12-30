04eb24406440e34c51d89621dfbdba2ac3ab6476
Suniti <72435330+suniti0804@users.noreply.github.com>
2021-12-30 17:49:30 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (75.32%) - LeetHub

--- reverse-integer/reverse-integer.cpp
+++ reverse-integer/reverse-integer.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     int reverse(int x) 
     {
-        long long rev=0;
+        long long res=0;
         while(x)
         {
-            rev=rev*10+(x%10);
-            if(rev>INT_MAX||rev<INT_MIN)
+            res=(res*10)+(x%10);
+            if(res>INT_MAX||res<INT_MIN)
                 return 0;
             x/=10;
         }
-        return rev;
+        return res;
+        
     }
 };
