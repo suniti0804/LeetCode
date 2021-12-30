e6843d7a172c851bbc8d1f787608af091cc85736
Suniti <72435330+suniti0804@users.noreply.github.com>
2021-12-30 19:04:36 +0530

    Time: 146 ms (29.77%), Space: 19.9 MB (86.11%) - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -2,40 +2,42 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) 
     {
-        if(nums.size() < 3)
-             return {};
-        sort(nums.begin(), nums.end());
+        if(nums.size()<3)
+            return {};
         vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
         
-        for(int i = 0; i < nums.size() - 2; i++)
+        for(int i=0; i<nums.size()-2; i++)
         {
-            if(i > 0 && nums[i] == nums[i - 1])
+            if(i>0&&nums[i]==nums[i-1])
                 continue;
+            int j=i+1;
+            int k=nums.size()-1;
             
-            int j = i + 1;
-            int k = nums.size() - 1;
-            while(j < k)
+            while(j<k)
             {
-               if(j > i + 1 && nums[j] == nums[j - 1])
-               {
-                   j++;
-                   continue;
-               }
-                if(k < nums.size() - 1 && nums[k] == nums[k + 1])
+                if(j>i+1&&nums[j]==nums[j-1])
+                {
+                    j++;
+                    continue;
+                }
+                
+                if(k<nums.size()-1&&nums[k]==nums[k+1])
                 {
                     k--;
                     continue;
                 }
-                if(nums[i] + nums[j] + nums[k] == 0)
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0)
                 {
                     res.push_back({nums[i], nums[j], nums[k]});
                     j++;
                     k--;
-                }
-                else if(nums[i] + nums[j] + nums[k] < 0)
-                    j++;
-                else
+                }    
+                else if(sum>0)
                     k--;
+                else
+                    j++;
             }
         }
         
