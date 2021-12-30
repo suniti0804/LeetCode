20565c367a81649c81803f928fc976777cb56c55
Suniti <72435330+suniti0804@users.noreply.github.com>
2021-12-30 17:55:14 +0530

    Time: 5 ms (11.50%), Space: 7.1 MB (51.40%) - LeetHub

--- string-to-integer-atoi/string-to-integer-atoi.cpp
+++ string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,43 +1,30 @@
 class Solution {
 public:
-    int myAtoi(string str) 
+    int myAtoi(string s) 
     {
-        long long res=0;
         int i=0;
-        while(str[i]==' ')
+        int mul=1;
+        while(s[i]==' ')
             i++;
-        if(i==str.size())
-            return 0;
-        int sign=1;
-        if(str[i]=='-')
+        if(s[i]=='-')
         {
-            sign=-1;
-            i++;
-        }    
-
-        else if(str[i]=='+')
-        {
-            sign=1;
+            mul=-1;
             i++;
         }
-
-        while(i<str.size())
+        
+        else if(s[i]=='+')
+            i++;
+        
+        long long res=0;
+        while(s[i]>='0'&&s[i]<='9')
         {
-            if(str[i]==' '||str[i]<'0'||str[i]>'9')
-                return res*sign;
-            res=res*10+(str[i]-'0');
+            res=(res*10)+(s[i]-'0');
             if(res>INT_MAX)
-            {
-                if(sign==-1)
-                    return INT_MIN;
-                else
-                    return INT_MAX;
-            }
+                return mul==-1?INT_MIN:INT_MAX;
             i++;
         }
         
-        return res*sign;
-        
+        return res*mul;
         
     }
 };
