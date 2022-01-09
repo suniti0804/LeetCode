a11603b31ce3173b6d523ca0f99a8b77784a6d51
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-01-09 18:20:12 +0530

    Added solution - LeetHub

---
+++ Positive Negative Pair - GFG/positive-negative-pair.cpp
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <math.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+// User function template for C++
+
+class Solution{
+  public:
+    //Function to return list containing all the pairs having both
+    //negative and positive values of a number in the array.
+    vector <int> findPairs(int arr[], int n) 
+    {
+        vector<int> res;
+        unordered_map<int, int> mp;
+        
+        for(int i=0; i<n; i++)
+        {
+            if(mp.find(-arr[i])!=mp.end())
+            {
+                res.push_back(-abs(arr[i]));
+                res.push_back(abs(arr[i]));
+            }
+            mp[arr[i]]++;  
+        }
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        vector <int> res = obj.findPairs(arr, n);
+    	if(res.size()!=0)
+    	{
+    		for (int i : res) 
+            	cout << i << " ";
+        	cout << endl;	
+    	}
+    	else
+    		cout<<0<<endl;
+    }
+
+    return 0;
+}  // } Driver Code Ends
