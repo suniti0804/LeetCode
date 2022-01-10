191dfb2610b16524f41d4e5c2aada0be0f97fb82
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-01-10 23:26:48 +0530

    Time: 7 ms (14.07%), Space: 8 MB (34.42%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.cpp
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -5,15 +5,17 @@ public:
         vector<string> res;
         for(int i=1; i<=n; i++)
         {
-            if(i%15==0)
-                res.push_back("FizzBuzz");
-            else if(i%5==0)
-                res.push_back("Buzz");
-            else if(i%3==0)
-                res.push_back("Fizz");
-            else
-                res.push_back(to_string(i));
+            string temp;
+            if(i%3==0)
+                temp+="Fizz";
+            if(i%5==0)
+                temp+="Buzz";
+            if(temp.empty())
+                temp+=to_string(i);
+            
+            res.push_back(temp);
         }
+        
         return res;
         
     }
