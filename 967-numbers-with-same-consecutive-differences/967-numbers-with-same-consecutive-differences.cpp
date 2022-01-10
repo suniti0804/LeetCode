fd21496346cf1f2b0f8d86770602427c11915abb
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-01-10 14:18:51 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (82.35%) - LeetHub

---
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    //jumping numbers, gfg
+    vector<int> numsSameConsecDiff(int n, int k) 
+    {
+        queue<int> q;
+        
+        for(int i=1;i<=9;i++)
+            q.push(i);
+
+        for(int i=1;i<n;i++)
+        {
+            int size = q.size();
+            for(int i=0;i<size;i++)
+            {
+                int num = q.front();
+                q.pop();
+                int last = num%10;
+                if(last+k<10)
+                    q.push(num*10+last+k);
+                if(last-k>=0 && k!=0)
+                    q.push(num*10+last-k);
+            }
+        }
+        
+        vector<int> ans;
+        if(n==1) 
+            ans.push_back(0);
+        while(!q.empty())
+        {
+            ans.push_back(q.front());
+            q.pop();
+        }
+        return ans;
+    }
+};
