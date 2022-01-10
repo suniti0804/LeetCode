3e5c62b14335ff8247e01d8e21a99797984805f5
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-01-10 21:32:22 +0530

    Added solution - LeetHub

--- 7. 3 Divisors - GFG/7.-3-divisors.cpp
+++ 7. 3 Divisors - GFG/7.-3-divisors.cpp
@@ -6,37 +6,31 @@ using namespace std;
 class Solution
 {
 public:
-
-vector<int> threeDivisors(vector<long long> query, int q)
+    vector<int> threeDivisors(vector<long long> query, int q)
     {
         vector<int> res;
         int n=1000001;
-        
-        int isPrime[n+1]; //addition of bool data type is not valid
-        for(int i=0; i<=n; i++)
-          isPrime[i]=1;
+        vector<int> isPrime(n+1, 1);
         
         isPrime[0]=isPrime[1]=0;
         
-        for(long long i=2; i*i<n; i++)
+        for(int i=2; i*i<=n; i++)
         {
             if(isPrime[i]==1)
             {
-                for(int p=2*i; p<=n; p+=i)
-                  isPrime[p]=0;
+                for(int j=2*i; j<=n; j+=i)
+                  isPrime[j]=0;
             }
         }
         
-        for(int i=1; i<=n; i++)
+        for(int i=2; i<=n; i++)
           isPrime[i]+=isPrime[i-1];
-        
-        for(int i=0; i<q; i++)
-          res.push_back(isPrime[(int)sqrt(query[i])]);    
+          
+        for(auto q:query)
+           res.push_back(isPrime[(int)sqrt(q)]);
         
         return res;
     }
-    
-    
 };
 
 // { Driver Code Starts.
