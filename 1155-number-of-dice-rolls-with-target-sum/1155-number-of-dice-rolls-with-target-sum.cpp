082dff949e49807977b89da398fcc3068978cca2
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-01-10 10:34:33 +0530

    Time: 174 ms (6.22%), Space: 8.5 MB (40.44%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -3,7 +3,7 @@ public:
     
     
     //recursion+memoization
-    /*int numRollsToTarget(int n, int k, int target) 
+    /*int numRollsToTarget(int n, int f, int target) 
     {
         int t[31][1001];
         memset(t, -1, sizeof(t));
@@ -17,8 +17,8 @@ public:
             return t[n][target];
         
         int res=0;
-        for(int i=1; i<=k; i++)
-            res+=numRollsToTarget(n-1, k, target-i);
+        for(int i=1; i<=f; i++)
+            res+=numRollsToTarget(n-1, f, target-i);
         
         return t[n][target]=res;
         
@@ -33,9 +33,9 @@ public:
         
         for(int i=1; i<=n; i++) //number of throws
         {
-            for(int j=1; j<=target; j++)  //current sum
+            for(int j=1; j<=target; j++) //current sum
             {
-                for(int k=1; k<=f; k++)     //face values
+                for(int k=1; k<=f; k++) //face values
                 {
                     if(k<=j)
                         t[i][j]=(t[i][j]%m+t[i-1][j-k]%m)%m;
