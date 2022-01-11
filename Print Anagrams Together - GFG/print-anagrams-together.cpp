e019d0e8dcabb01003940a2d7b9fc94741383051
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-01-11 15:30:25 +0530

    Added solution - LeetHub

---
+++ Print Anagrams Together - GFG/print-anagrams-together.cpp
@@ -0,0 +1,60 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& list)
+    {
+        vector<vector<string>> res;
+        unordered_map<string, vector<string>> mp;
+        
+        for(auto s:list)
+        {
+            string curr=s;
+            sort(curr.begin(), curr.end());
+            mp[curr].push_back(s);
+        }
+        
+        for(auto i:mp)
+          res.push_back(i.second);
+          
+        return res;  
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<string> string_list(n);
+        for (int i = 0; i < n; ++i)
+            cin>>string_list[i]; 
+        Solution ob;
+        vector<vector<string> > result = ob.Anagrams(string_list);
+        sort(result.begin(),result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for(int j=0; j < result[i].size(); j++)
+            {
+                cout<<result[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
