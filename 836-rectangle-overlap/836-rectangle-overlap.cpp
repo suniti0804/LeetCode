3e09879c16ba4ede4ce8143db19fc5b6f4a96c94
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-01-12 10:03:15 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (15.76%) - LeetHub

--- 836-rectangle-overlap/836-rectangle-overlap.cpp
+++ 836-rectangle-overlap/836-rectangle-overlap.cpp
@@ -2,9 +2,16 @@ class Solution {
 public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) 
     {
-        if(rec1[2]<=rec2[0]||rec2[2]<=rec1[0]||rec1[3]<=rec2[1]||rec2[3]<=rec1[1])
+        /*if(rec1[2]<=rec2[0]||rec2[2]<=rec1[0]||rec1[3]<=rec2[1]||rec2[3]<=rec1[1])
             return false;
-        return true;
+        return true; */
+        
+        int x1=max(rec1[0], rec2[0]);
+        int y1=max(rec1[1], rec2[1]);
+        int x2=min(rec1[2], rec2[2]);
+        int y2=min(rec1[3], rec2[3]);
+        
+        return (x1<x2&&y1<y2);
         
     }
 };
