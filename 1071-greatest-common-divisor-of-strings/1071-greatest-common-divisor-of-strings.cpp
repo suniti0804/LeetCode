66b0f5dcdfc8ee22de3dd182295d23fcdc5a233d
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-01-13 12:00:02 +0530

    Time: 3 ms (79.81%), Space: 7.3 MB (37.54%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,40 +1,14 @@
 class Solution {
 public:
-    
-    bool solve(string a, string b)
-    {
-        int i=0;
-        int k=0;
-        while(i<a.size())
-        {
-            if(k==b.size())
-                k=0;
-            while(a[i]==b[k]&&i<a.size()&&k<b.size())
-            {
-                i++;
-                k++;
-            }
-            
-            if(k!=b.size())
-                return false;
-            
-        }
-        
-        return true;
-    }
-    
     string gcdOfStrings(string str1, string str2) 
     {
         int a=str1.size();
         int b=str2.size();
         
-        int len=__gcd(a,b);
-        
-        if(str1+str2!=str2+str1)
+        if(str1+str2!=str2+str1)  //nice logic!!
             return "";
         
-        string res=str1.substr(0,len);
-            return res;
+        return str1.substr(0, __gcd(a,b));
         
     }
 };
