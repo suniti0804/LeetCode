3287953472568ac40bed6c7aea5a862b32017abb
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-01-13 10:33:48 +0530

    Time: 10 ms (94.17%), Space: 6 MB (91.96%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int numSquares(int n) 
+   /* int numSquares(int n) 
     {
         vector<int> t(n+1, INT_MAX);
         
@@ -17,5 +17,21 @@ public:
         }
     
         return t[n];
-    }
+    }  */
+    
+    int is_square(int n){  
+        int temp = (int) sqrt(n);  
+        return temp * temp == n;  
+    }  
+    int numSquares(int n) {  
+        while ((n & 3) == 0) //n%4 == 0  
+            n >>= 2;  
+        if ((n & 7) == 7) return 4; //n % 8 == 7  
+        if(is_square(n)) return 1;  
+        int sqrt_n = (int) sqrt(n);  
+        for(int i = 1; i<= sqrt_n; i++){  
+            if (is_square(n-i*i)) return 2;  
+        }  
+        return 3;  
+    }  
 };
