eb51d44f29d3e0efde0e3c59340377aa43c747bb
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-01-15 08:46:15 +0530

    Time: 11 ms (52.11%), Space: 9.8 MB (35.65%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     
-    void solve(vector<int>& nums, int s, int e, vector<vector<int>>& res)
-    {
-        if(s == e)
-          res.push_back(nums);
-        
-        else
-        {
-            for(int i = s; i <= e; i++)
-            {
-                swap(nums[s], nums[i]); //swapping
-                solve(nums, s + 1, e, res); //recursion
-                swap(nums[s], nums[i]); //backtrack
-            }
+    vector<vector<int>> res;
+    void permuteRec(vector<int> nums, int pos) {
+        int n = nums.size();
+        if (pos == n) {
+            res.push_back(nums);
+            return;
+        }
+        for (int i = pos; i < n; ++i) {
+            if (i != pos && nums[i] == nums[pos]) continue;
+            swap(nums[pos], nums[i]);
+            permuteRec(nums, pos + 1);
         }
         
-        return;
     }
-    
-    
-    vector<vector<int>> permuteUnique(vector<int>& nums) 
-    {
-        vector<vector<int>> res;
-        solve(nums, 0, nums.size()-1, res);
-        sort(res.begin(), res.end());
-        
-        res.erase(unique(res.begin(), res.end()), res.end());
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        permuteRec(nums, 0);
         return res;
-        
     }
+    
+    
 };
