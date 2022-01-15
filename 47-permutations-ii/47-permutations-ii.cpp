f81c52e2296b41eeda5ec7fb13525faf60341a60
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-01-15 08:50:38 +0530

    Time: 12 ms (50.01%), Space: 9.6 MB (42.15%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -2,24 +2,32 @@ class Solution {
 public:
     
     vector<vector<int>> res;
-    void permuteRec(vector<int> nums, int pos) {
-        int n = nums.size();
-        if (pos == n) {
-            res.push_back(nums);
+    
+    void solve(vector<int> nums, int s, int e)
+    {
+        if(s==e)
+        {
+            res.push_back(nums);   
             return;
+        }    
+        
+        
+        for(int i=s; i<=e; i++)
+        {
+            if(i!=s&&nums[i]==nums[s])
+                continue;
+            swap(nums[i], nums[s]);
+            solve(nums, s+1, e);
         }
-        for (int i = pos; i < n; ++i) {
-            if (i != pos && nums[i] == nums[pos]) continue;
-            swap(nums[pos], nums[i]);
-            permuteRec(nums, pos + 1);
-        }
+    
+    }
+    
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums) 
+    {
+        sort(nums.begin(), nums.end());
+        solve(nums, 0, nums.size()-1);
+        return res;
         
     }
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        permuteRec(nums, 0);
-        return res;
-    }
-    
-    
 };
