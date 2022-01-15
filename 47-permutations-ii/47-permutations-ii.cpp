4aa3b09dc4e78c76834da22cebbdc1ab5320d597
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-01-15 08:53:16 +0530

    Time: 8 ms (72.28%), Space: 9.1 MB (64.89%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     
-    vector<vector<int>> res;
+    //passing &nums doesn't changes the vector thus swapping was not
+    //happening and thus was getting WA
     
-    void solve(vector<int> nums, int s, int e)
+    void solve(vector<int> nums, int s, int e, vector<vector<int>>& res)
     {
         if(s==e)
         {
@@ -11,22 +12,26 @@ public:
             return;
         }    
         
-        
-        for(int i=s; i<=e; i++)
+        else
         {
-            if(i!=s&&nums[i]==nums[s])
-                continue;
-            swap(nums[i], nums[s]);
-            solve(nums, s+1, e);
+            for(int i=s; i<=e; i++)
+            {
+                if(i!=s&&nums[i]==nums[s])
+                    continue;
+                swap(nums[i], nums[s]);
+                solve(nums, s+1, e, res);
+            }
         }
-    
+        
+        return;
     }
     
     
     vector<vector<int>> permuteUnique(vector<int>& nums) 
     {
+        vector<vector<int>> res;
         sort(nums.begin(), nums.end());
-        solve(nums, 0, nums.size()-1);
+        solve(nums, 0, nums.size()-1, res);
         return res;
         
     }
