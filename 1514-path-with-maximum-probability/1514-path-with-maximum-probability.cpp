3b2b0a641954f42a4fe6e4a45b28fd80f87729e9
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-01-26 20:41:43 +0530

    Time: 345 ms (23.93%), Space: 66.1 MB (58.94%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& prob, int start, int end) 
     {
+        //create a weighted graph
         vector<vector<pair<int, double>>> adj(n);
         for(int i=0; i<edges.size(); i++) 
         {
@@ -9,8 +10,9 @@ public:
             adj[edges[i][1]].push_back({edges[i][0], prob[i]});   
         }
         
+        //initialize values
         vector<double> dist(n, (double)0.0);
-        priority_queue<pair<double, int>> pq;
+        priority_queue<pair<double, int>> pq; //use of max heap
         pq.push({1.0, start});
         dist[start]=1.0;
         
