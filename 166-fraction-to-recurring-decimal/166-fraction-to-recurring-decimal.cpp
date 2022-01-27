90de198fde2eda76fea55f135f25dfae308b1636
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-01-27 10:29:31 +0530

    Time: 5 ms (12.77%), Space: 6.3 MB (79.00%) - LeetHub

--- 166-fraction-to-recurring-decimal/166-fraction-to-recurring-decimal.cpp
+++ 166-fraction-to-recurring-decimal/166-fraction-to-recurring-decimal.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
-    string fractionToDecimal(int n, int d) 
+    string fractionToDecimal(int numerator, int denominator) 
     {
-        if(n==0)
-            return "0";
         string res="";
-        if (n>0^d>0) 
+        if(numerator==0)
+            return "0";
+        if(numerator>0^denominator>0)
             res+='-';
+        long num=abs(numerator);
+        long den=abs(denominator);
+        unordered_map<long, int> mp;
         
-        long long num=abs(n);
-        long long den=abs(d);
+        res+=to_string(num/den);
+        if(num%den==0)
+            return res;
+        res+='.';
         
-       res+=to_string(num/den);
-       long long quotient=0;
-       long long rem=0;
-       unordered_map<long long,int> mp;
-       if(num%den==0)
-         return res;
-
-       res+=".";
-       rem=num%den;
-
-       while(rem != 0)
+        long rem=num%den;
+        
+        while(rem)
         {
-            if(mp.find(rem) != mp.end())
-              {
-                  int len = mp[rem];
-                  res.insert(len, "(");
-                  res += ")";
-                  break;
-              }
+            if(mp.find(rem)!=mp.end())
+            {
+                res.insert(mp[rem], "(");
+                res+=')';
+                break;
+            }
             
-               mp[rem]=res.length();
-               rem*=10;
-               quotient=rem/den;
-               res+=to_string(quotient);
-               rem%=den;
-             
+            mp[rem]=res.size();
+            rem*=10;
+            res+=to_string(rem/den);
+            rem%=den;
         }
-
-       return res;
-
+        
+        return res;
+        
     }
 };
