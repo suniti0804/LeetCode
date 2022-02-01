f173e9c27b76545035c5f26655fc1964ba41101d
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-02-01 12:13:37 +0530

    Time: 539 ms (5.02%), Space: 49.4 MB (15.50%) - LeetHub

---
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,23 @@
+typedef pair<int, int> pii;
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) 
+    {
+        vector<int> dist(n, INT_MAX);
+        dist[src] = 0;
+        
+        // Run only K+1 times since we want shortest distance in K hops
+        for( int i=0; i<=k; i++) 
+        {
+            vector<int> temp(dist);
+            for(auto i:flights) 
+            {
+                if(dist[i[0]]!=INT_MAX) 
+                    temp[i[1]]=min(temp[i[1]], dist[i[0]]+i[2]);
+                
+            }
+            dist=temp;
+        }
+        return dist[dst]==INT_MAX?-1:dist[dst];
+    }
+};
