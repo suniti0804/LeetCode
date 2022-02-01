ef149d16579b860ca5314fca0be63afe068c4f1f
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-02-01 09:17:19 +0530

    Time: 313 ms (37.70%), Space: 66.1 MB (57.84%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -2,35 +2,33 @@ class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& prob, int start, int end) 
     {
-        //create a weighted graph
-        vector<vector<pair<int, double>>> adj(n);
-        for(int i=0; i<edges.size(); i++) 
-        {
-            adj[edges[i][0]].push_back({edges[i][1], prob[i]});
-            adj[edges[i][1]].push_back({edges[i][0], prob[i]});   
+        vector<vector<pair<int,double>>> adj(n);
+        for(int i=0; i<edges.size(); i++)
+        {   
+            adj[edges[i][0]].push_back({edges[i][1],prob[i]});
+            adj[edges[i][1]].push_back({edges[i][0],prob[i]});
         }
         
-        //initialize values
-        vector<double> dist(n, (double)0.0);
-        priority_queue<pair<double, int>> pq; //use of max heap
-        pq.push({1.0, start});
-        dist[start]=1.0;
+        vector<double> res(n, (double)0.0);
+        priority_queue<pair<double,int>> pq;
+        pq.push({1.0,start});
+        res[start]=1.0;
         
         while(!pq.empty())
         {
             int u=pq.top().second;
             pq.pop();
+            
             for(auto v:adj[u])
             {
-                if(dist[u]*v.second>dist[v.first])
+                if(res[u]*v.second>res[v.first])
                 {
-                    dist[v.first]=dist[u]*v.second;
-                    pq.push({dist[v.first], v.first});
+                    res[v.first]=res[u]*v.second;
+                    pq.push({res[v.first],v.first});
                 }
             }
         }
         
-        return dist[end];
-        
+        return res[end];
     }
 };
