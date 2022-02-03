dfaee588ff57c23b046e60a88812b5782d078954
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-02-03 19:18:34 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (76.97%) - LeetHub

---
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string convertToTitle(int n) 
+    {
+        string res;
+        char tmp;
+        while(n)
+        {
+           n -= 1;
+           tmp = 'A' + n % 26;
+           //res += tmp;
+           res = tmp + res;
+           n /= 26;
+        }
+
+         return res;
+        
+    }
+};
