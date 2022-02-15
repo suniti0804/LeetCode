d7d9197ef53ed776727b6d37c22f4c22fac5b57a
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-02-15 10:11:22 +0530

    Time: 29 ms (52.04%), Space: 14.3 MB (40.22%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    bool isCyclic(int u, vector<int> adj[], vector<int>& visited)
+    {
+        if(visited[u]==1)
+            return true;
+
+        if(visited[u]==0)
+        {
+            visited[u]=1;
+            for(auto v:adj[u])
+                if(isCyclic(v, adj, visited))
+                    return true;
+        }
+        
+        visited[u]=2;
+        return false;
+    }
+    
+    bool canFinish(int n, vector<vector<int>>& edges) 
+    {
+        vector<int> adj[n+1];
+        
+        for(auto e:edges)
+            adj[e[0]].push_back(e[1]);
+        
+        vector<int> visited(n, 0);
+        
+        for(int i=0; i<n; i++)
+        {
+            if(isCyclic(i, adj, visited))
+                return false;
+        }
+        
+        return true;
+    }
+};
