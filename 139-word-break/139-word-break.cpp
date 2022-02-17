25ad220a52b79f48ccba5c05c857664d5e4da056
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-02-17 12:14:57 +0530

    Time: 4 ms (91.02%), Space: 9.4 MB (68.19%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& dict)
+    {
+        int n=s.size();
+        vector<bool> t(n+1, false);
+        t[0]=true;
+        
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=i-1; j>=0; j--)
+            {
+                if(t[j])
+                {
+                    string str=s.substr(j, i-j);
+                    auto it=find(dict.begin(), dict.end(), str);
+                    if(it!=dict.end())
+                    {
+                        t[i]=true;
+                        break;
+                    }
+                }
+                
+            }
+        }
+        return t[n];
+    }
+};
