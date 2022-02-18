5213b097b8cd1e36055a2d74c0c4c77323fd5440
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-02-18 21:17:24 +0530

    Time: 5 ms (58.25%), Space: 7 MB (52.75%) - LeetHub

---
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) 
+    {
+        string ans = "";                                         
+        
+       
+       for (char c : num) {
+           while (ans.length() && ans.back() > c && k) {
+               ans.pop_back();                                  // make sure digits in ans are in ascending order
+               k--;                                             // remove one char
+           }
+           
+           if (ans.length() || c != '0') { ans.push_back(c); }  // can't have leading '0'
+       }
+       
+       while (ans.length() && k--) { ans.pop_back(); }          // make sure remove k digits in total
+       
+       return ans.empty() ? "0" : ans;
+    }
+};
