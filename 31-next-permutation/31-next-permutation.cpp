3802e12994671413ba6eb742e9147c00b52481de
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-02-20 01:44:21 +0530

    Time: 9 ms (26.73%), Space: 12.1 MB (36.81%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -6,27 +6,30 @@ public:
         int i;
         for(i = N - 1; i > 0; i--)
          {
-             if(arr[i] > arr[i - 1])
+             if(arr[i]>arr[i - 1])
                 break;
          }
          
-        if(i == 0)
+        if(i==0)
          {
              reverse(arr.begin(), arr.end());
              return;
+            //return arr;
          }
         
         int x = arr[i - 1], smallest = i;
+        cout<<"x is: "<<x<<"\n";
         for(int j = i + 1; j < N; j++)
          {
-             if(arr[j] > x && arr[j] < arr[smallest])
-               smallest = j;
+             if(arr[j]>x&&arr[j]<arr[smallest])
+               smallest=j;
          }
          
-        swap(arr[smallest], arr[i - 1]);
-        sort(arr.begin() + i, arr.end());
+        swap(arr[smallest], arr[i-1]);
+        sort(arr.begin()+i, arr.end());
         
         return;
+        //return arr;
          
     }
 };
