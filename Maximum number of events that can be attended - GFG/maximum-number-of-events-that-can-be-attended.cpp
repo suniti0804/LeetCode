daa69e7e1a6ab7d39f8c655478fd39fb41431a23
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-02-21 17:28:38 +0530

    Added solution - LeetHub

--- Maximum number of events that can be attended - GFG/maximum-number-of-events-that-can-be-attended.cpp
+++ Maximum number of events that can be attended - GFG/maximum-number-of-events-that-can-be-attended.cpp
@@ -8,38 +8,43 @@ using namespace std;
 
 class Solution {
   public:
-    int maxEvents(int start[], int end[], int N)
+    int maxEvents(int start[], int end[], int n) 
     {
-       vector<pair<int,int>>events;
-       for(int i=0;i<N;i++)
-           events.push_back({start[i], end[i]});
-       
-       sort(events.begin(), events.end());
-       
-       priority_queue<int,vector<int>, greater<int>>pq;
-       
-       int cnt=0;
-       int i=0, d=0;
-       while(!pq.empty()||i<N)
-       {
-           if(pq.size()==0)
-              d=events[i].first;
-           
-           while(i<N && events[i].first==d) //<=d, but as sorted so no value 
-                                            //can be greater than d
-              pq.push(events[i++].second);
-             
-           pq.pop();
-             
-           cnt++;
-           d++;
-           while(!pq.empty()&&pq.top()<d) 
+        vector<pair<int, int>> event;
+        
+        for(int i=0; i<n; i++)
+          event.push_back({start[i], end[i]});
+        
+        sort(event.begin(), event.end());
+        
+        priority_queue<int, vector<int>, greater<int>> pq;
+        
+        int i=0, cnt=0, d=0;
+        
+        while(!pq.empty()||i<n)
+        {
+            if(pq.empty())
+               d=event[i].first;
+            
+            while(i<n&&event[i].first==d)
+            {
+                pq.push(event[i].second);
+                i++;
+            }
+            
+            pq.pop();
+            
+            cnt++;
+            d++;
+            
+            while(!pq.empty()&&pq.top()<d)
               pq.pop();
-       }
-       
-       return cnt;
+        }
+        
+        return cnt;
+        
     }
-};    
+};
 
 // { Driver Code Starts.
 
