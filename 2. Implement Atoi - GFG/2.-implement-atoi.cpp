825c5a1a40496c5fae0fdf836f74ef29f4a1e88f
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-02-25 23:01:05 +0530

    Added solution - LeetHub

---
+++ 2. Implement Atoi - GFG/2.-implement-atoi.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    /*You are required to complete this method */
+    int atoi(string str) {
+        int res=0;
+        int i=0;
+        if(str[i]=='-'||str[i]=='+')
+          i++;
+        
+        while(str[i]>='0'&&str[i]<='9')
+        {
+            res=(res*10)+(str[i]-'0');
+            i++;
+        }
+        
+        if(i==str.size())
+          return str[0]=='-'?(-1)*res:res;
+        return -1;  
+
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string s;
+		cin>>s;
+		Solution ob;
+		cout<<ob.atoi(s)<<endl;
+	}
+}  // } Driver Code Ends
