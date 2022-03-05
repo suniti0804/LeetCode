5037894a0956de448043192c31e162e288e52b2c
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-03-05 16:01:16 +0530

    Time: 20 ms (96.13%), Space: 12.3 MB (31.77%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -3,27 +3,32 @@ public:
     int minimumMountainRemovals(vector<int>& arr) 
     {
         int n=arr.size();
-        vector<int> t1(n, 1), t2(n, 1);
-        
-        for(int i=0; i<n; i++)
-            for(int j=0; j<i; j++)
-                if(arr[i]>arr[j])
-                    t1[i]=max(t1[i], t1[j]+1);
-        
-        for(int i=n-1; i>=0; i--)
-            for(int j=n-1; j>i; j--)
-                if(arr[i]>arr[j])
-                    t2[i]=max(t2[i], t2[j]+1);
-        
-        int res=0;
+        vector<int> t1, t2, t(n);
+        int res=INT_MAX;
         
         for(int i=0; i<n; i++)
         {
-            if(t1[i]==1||t2[i]==1)
-                continue;
-            res=max(res, t1[i]+t2[i]-1);
+            auto it=lower_bound(t1.begin(), t1.end(), arr[i]);
+            if(it==t1.end())
+                t1.push_back(arr[i]);
+            else
+                *it=arr[i];
+            t[i]=t1.size();
         }
-      
-        return n-res;
+        
+        for(int i=n-1; i>=0; i--)
+        {
+            auto it=lower_bound(t2.begin(), t2.end(), arr[i]);
+            if(it==t2.end())
+                t2.push_back(arr[i]);
+            else
+                *it=arr[i];
+            
+            if(t[i]>1&&t2.size()>1)
+                res=min(res, n-(t[i]+(int)t2.size()-1));
+                
+        }
+        
+        return res;
     }
 };
