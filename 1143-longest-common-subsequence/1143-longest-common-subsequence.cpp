a2eae07f1bfb7e7c59f65ed9dbd2a276b3af0e28
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-03-05 19:36:10 +0530

    Time: 20 ms (70.44%), Space: 10.7 MB (76.23%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -2,27 +2,25 @@ class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) 
     {
-        int m = text1.length();
-        int n = text2.length();
+        int n=text1.size();
+        int m=text2.size();
         
-        int t[m + 1][n + 1];
+        int t[n+1][m+1];
         
-        for(int i = 0; i <= m; i++)
+        for(int i=0; i<=n; i++)
         {
-            for(int j = 0; j <= n; j++)
+            for(int j=0; j<=m; j++)
             {
-                if(i == 0 || j == 0)
-                    t[i][j] = 0;
-                else if(text1[i - 1] == text2[j - 1]) //else has to be used
-                    t[i][j] = 1 + t[i - 1][j - 1];
+                if(i==0||j==0)
+                    t[i][j]=0;
+                else if(text1[i-1]==text2[j-1])
+                    t[i][j]=t[i-1][j-1]+1;
                 else
-                    t[i][j] = max(t[i - 1][j], t[i][j - 1]);
+                    t[i][j]=max(t[i-1][j], t[i][j-1]);
             }
         }
-       return t[m][n];
-       //for longest common substring replace 
-       //'max(t[i - 1][j], t[i][j - 1])' with 0 because in substring
-       //continuous length is required.
+        
+        return t[n][m];
         
     }
 };
