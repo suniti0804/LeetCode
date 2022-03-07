9ac49114554f2e70efc1ce6ca34ce750d4590e78
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-03-07 13:00:43 +0530

    Time: 16 ms (27.27%), Space: 10.6 MB (14.81%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -9,7 +9,7 @@ public:
         for(int i=0; i<n; i++)
         {
             sum+=nums[i];
-            while(sum>=target&&j<=i)
+            while(sum>=target)
             {
                 res=min(res, i-j+1);
                 sum-=nums[j++];
