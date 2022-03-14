8aff3fccfed875cdf2361069a4e040d714919edc
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-03-14 10:06:32 +0530

    Time: 24 ms (37.68%), Space: 13.2 MB (81.82%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -5,26 +5,32 @@ public:
         int m=matrix.size();
         int n=matrix[0].size();
         
-        vector<int> row(m, 0), col(n, 0);
+        bool row=false, col=false;
         
         for(int i=0; i<m; i++)
             for(int j=0; j<n; j++)
-            {
                 if(matrix[i][j]==0)
                 {
-                    row[i]=1;
-                    col[j]=1;
+                    if(i==0)
+                        row=true;
+                    if(j==0)
+                        col=true;
+                    matrix[i][0]=matrix[0][j]=0;
                 }
-            }
         
-        for(int i=0; i<m; i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                if(row[i]==1||col[j]==1)
+        for(int i=1; i<m; i++)
+            for(int j=1; j<n; j++)
+                if(matrix[i][0]==0||matrix[0][j]==0)
                     matrix[i][j]=0;
-            }
+        if(row)
+        {
+            for(int i=0; i<n; i++)
+                matrix[0][i]=0;
         }
-        
+        if(col)
+        {
+            for(int i=0; i<m; i++)
+                matrix[i][0]=0;
+        }
     }
 };
