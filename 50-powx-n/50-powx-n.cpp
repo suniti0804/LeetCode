3903eb29096119bff81d1b2ada5e8562d1bbf410
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-03-14 13:38:46 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (81.57%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     double myPow(double x, int n) 
     {
-        if(n==0)
-            return 1;
-        double res=myPow(x, n/2);
-        if(n%2==0)
-            return res*res;
-        else
-            return n>0?x*res*res:res*res/x;
-        
+        double ans = 1; // Initialize result
+        long y=abs((long)n);
+        while (y > 0) 
+        {
+            // If y is odd, multiply x with result
+            if (y & 1)
+                ans = ans * x;
+            y/=2;
+            x = x * x; // Change x to x^2
+        }
+        return n < 0 ? 1/ans : ans;
     }
 };
