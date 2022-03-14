0e3c0ef5494091e1d9f82ad0c3b64b960e88abe4
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-03-14 12:01:40 +0530

    Time: 6 ms (27.76%), Space: 9.1 MB (73.05%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -7,9 +7,10 @@ public:
         
         while(i>=0&&j>=0)
             nums1[k--]=nums1[i]>nums2[j]?nums1[i--]:nums2[j--];
-        while(i>=0)
-            nums1[k--]=nums1[i--];
         while(j>=0)
             nums1[k--]=nums2[j--];
+        while(i>=0)
+            nums1[k--]=nums1[i--];
+        
     }
 };
