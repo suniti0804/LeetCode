dc4811a58f73a1045a23be3e19564b07bd67bdb2
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-03-28 00:12:19 +0530

    Time: 21 ms (33.96%), Space: 18.3 MB (15.32%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    int trap(vector<int>& height) 
+    int trap(vector<int>& arr) 
     {
-         //Dynamic Programming Approach
-        int n = height.size();
-        if(n == 0 || n == 1)
-            return 0;
+        int n=arr.size();
+        int res=0;
+        vector<int> left(n), right(n);
+        left[0]=arr[0];
         
-        int right[n], left[n];
+        for(int i=1; i<n; i++)
+            left[i]=max(left[i-1], arr[i]);
         
-        left[0] = height[0];
+        right[n-1]=arr[n-1];
+        for(int i=n-2; i>=0; i--)
+            right[i]=max(right[i+1], arr[i]);
         
-        for(int i = 1; i < n; i++)
-            left[i] = max(height[i], left[i - 1]);
-        
-        right[n - 1] = height[n - 1];
-        
-        for(int i = n - 2; i >= 0; i--)
-            right[i] = max(height[i], right[i + 1]);
-        
-        int res = 0;
-        for(int i = 0; i < n; i++)
-            res += min(right[i], left[i]) - height[i];
+        for(int i=0; i<n; i++)
+            res+=min(left[i], right[i])-arr[i];
         
         return res;
     }
