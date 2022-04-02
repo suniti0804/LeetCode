928d7155b6ac205d47dcf7ab8b05b6298d17d683
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-04-02 11:37:59 +0530

    Time: 30 ms (52.83%), Space: 14.4 MB (39.51%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    
+    bool isCycle(int v, vector<int> adj[], vector<int>& vis, vector<int>& res)
+	{
+        vis[v]=1;
+        for(auto u:adj[v])
+        {
+            if(vis[u]==1)
+                return true;
+            
+            if(vis[u]==0)
+              if(isCycle(u, adj, vis, res))
+                  return true;
+        }
+
+        vis[v]=2;
+        res.push_back(v);
+        return false;
+	}
+    
+    vector<int> findOrder(int num, vector<vector<int>>& edges) 
+    {
+        vector<int> res;
+        vector<int> vis(num, 0);
+        stack<int> st;
+        
+        int n=edges.size();
+        vector<int> adj[num+1]; //vector<vector<int>> adj(n+1);
+        
+        for(auto e:edges)
+            adj[e[0]].push_back(e[1]);
+	    
+	    for(int i=0; i<num; i++)
+	    {
+	        if(!vis[i])
+	          if(isCycle(i, adj, vis, res))
+                  return {};
+	    }
+	    
+	   return res;
+    }
+};
