acea939deaf02dc505e52e0512dfd58038894e90
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-04-04 16:21:55 +0530

    Time: 12 ms (13.02%), Space: 10.6 MB (96.33%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -14,16 +14,17 @@ public:
     {
         ListNode* temp=new ListNode(-1);
         temp->next=head;
-        ListNode* fast=temp;
+        ListNode*fast=temp;
         ListNode* slow=temp;
-        
         while(n--)
             fast=fast->next;
+       
         while(fast&&fast->next)
         {
             fast=fast->next;
             slow=slow->next;
         }
+        
         slow->next=slow->next->next;
         return temp->next;
     }
