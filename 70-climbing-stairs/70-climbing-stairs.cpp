eab1f5e6afceed69d5ba619513e976a3884a8bd4
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-04-07 13:48:17 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (55.57%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -4,12 +4,12 @@ public:
     {
         int t[n+1];
         t[0]=1;
-        t[1]=2;
+        t[1]=1;
         
-        for(int i=2; i<n; i++)
+        for(int i=2; i<=n; i++)
             t[i]=t[i-1]+t[i-2];
         
-        return t[n-1];
+        return t[n];
         
     }
 };
