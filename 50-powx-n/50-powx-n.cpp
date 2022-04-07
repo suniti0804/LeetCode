f11c999e955a0fa628c0045b8f6b342191366c6a
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-04-07 13:41:57 +0530

    Time: 2 ms (43.22%), Space: 5.9 MB (45.05%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -3,16 +3,16 @@ public:
     double myPow(double x, int n) 
     {
         double res=1;
-        long y=abs((long)(n));
+        long y=abs((long)n);
         
         while(y)
         {
             if(y&1)
                 res*=x;
-            x=x*x;
+            x*=x;
             y>>=1;
         }
         
-        return n>0?res:(1/res);
+        return n<0?(1/res):res;
     }
 };
