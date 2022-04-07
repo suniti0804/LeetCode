079906f63beba12d161467cc8a8bf7be41bac5af
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-04-07 14:19:48 +0530

    Time: 862 ms (22.00%), Space: 7.9 MB (74.94%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -9,7 +9,7 @@ public:
         if(i<0||i>=board.size()||j<0||j>=board[i].size()||board[i][j]!=word[idx]) 
             return false;
         
-        board[i][j] = '0';
+        board[i][j]='0';  //because character can't be reused
         idx++;
         
         bool status =   dfs(board, word, i+1, j, idx) ||  //down
@@ -17,7 +17,7 @@ public:
                         dfs(board, word, i-1, j, idx) ||  //up
                         dfs(board, word, i, j-1, idx);  //left
         
-        board[i][j]=word[idx-1];
+        board[i][j]=word[idx-1]; //restore the character
 		
         return status;
     }
