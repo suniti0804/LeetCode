07d9ee6254f3c2afe97e6cdd3e64ebbbc672993a
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-04-09 12:22:27 +0530

    Time: 0 ms (100.00%), Space: 6 MB (73.56%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -3,16 +3,18 @@ public:
     int uniquePaths(int m, int n) 
     {
         int t[m][n];
+        
         for(int i=0; i<m; i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                if(i==0||j==0)
-                    t[i][j]=1;
-                else 
+            t[i][0]=1;
+        
+        for(int j=0; j<n; j++)
+            t[0][j]=1;
+        
+        for(int i=1; i<m; i++)
+            for(int j=1; j<n; j++)
                     t[i][j]=t[i-1][j]+t[i][j-1];
-            }
-        }    
+            
+          
         return t[m-1][n-1];  
     }
 };
