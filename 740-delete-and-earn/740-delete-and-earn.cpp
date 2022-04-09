d1a37cebc85377b5c7144b3654a3aad15e4e8fbf
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-04-09 14:40:51 +0530

    Time: 15 ms (41.55%), Space: 13.5 MB (32.18%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
+    //very similar to maximum sum of non adjacent elements
     int deleteAndEarn(vector<int>& nums) 
     {
-        int n = nums.size();
+        int n=nums.size();
         unordered_map<int, int> mp;
-        for(int i = 0; i < n; i++)
-            mp[nums[i]] += nums[i];
+        for(auto num:nums)
+            mp[num]+=num;  //to have the count of maximum point this num can give
         
-        int N = *max_element(nums.begin(), nums.end());
+        int N=*max_element(nums.begin(), nums.end());
+        
         vector<int> t(N+1, 0);
-        t[1] = mp[1];
+        t[1]=mp[1];
         
-        for(int i = 2; i <= N; i++)
-            t[i] = max(mp[i] + t[i - 2], t[i - 1]);
+        for(int i=2; i<=N; i++)
+            t[i]=max(mp[i]+t[i-2], t[i-1]);
         
         return t[N];
         
