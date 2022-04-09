83fa74251662d3e817be275bfd2ae19ebdc33824
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-04-09 12:09:44 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (48.24%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -5,18 +5,18 @@ public:
         int n=nums.size();
         if(n<3)
             return 0;
+        
         int res=0;
         int ctr=0;
         
         for(int i=2; i<nums.size(); i++)
         {
             if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2])
-            {
                 ctr++;
-                res+=ctr;
-            }
             else
                 ctr=0;
+            
+            res+=ctr;
         }
         
         return res;
