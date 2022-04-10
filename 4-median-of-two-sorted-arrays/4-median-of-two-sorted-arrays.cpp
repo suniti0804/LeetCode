4fda73634328275ea9d68f0c1786881360125306
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-04-10 09:56:11 +0530

    Time: 24 ms (97.02%), Space: 89.4 MB (48.79%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -37,21 +37,21 @@ public:
     {
         if(a1.size()>a2.size())
             swap(a1, a2);
-        int n1=a1.size();
-        int n2=a2.size();
-        int total=(n1+n2);
+        int m=a1.size();
+        int n=a2.size();
+        int total=m+n;
         
-        int lo=0, hi=n1;
+        int lo=0, hi=m;
         
         while(lo<=hi)
         {
             int mid1=(lo+hi)/2;
-            int mid2=(total)/2-mid1;
+            int mid2=((total)/2)-mid1;
             
             int l1=(mid1==0)?INT_MIN:a1[mid1-1];
             int l2=(mid2==0)?INT_MIN:a2[mid2-1];
-            int r1=(mid1==n1)?INT_MAX:a1[mid1];
-            int r2=(mid2==n2)?INT_MAX:a2[mid2];
+            int r1=(mid1==m)?INT_MAX:a1[mid1];
+            int r2=(mid2==n)?INT_MAX:a2[mid2];
             
             if(l1>r2)
                 hi=mid1-1;
@@ -61,10 +61,11 @@ public:
             {
                 if(total%2==0)
                     return (max(l1,l2)+min(r1,r2))/2.;
-                else
-                    return min(r1,r2);
+                return min(r1,r2);
             }
         }
+        
         return -1;
+        
     }
 };
