9d3c901dae63e3050bac440b4db628efee869057
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-04-11 00:02:52 +0530

    Time: 161 ms (36.80%), Space: 13.5 MB (86.13%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,109 @@
+class Solution {
+public:
+    
+    bool isValid(string a, string b)
+    {
+        int m=a.size();
+        int n=b.size();
+        
+        if((m+1)!=n)
+            return false;
+        
+        bool flag=false; //to mark missing element
+   
+        int j=0;
+        for(int i=0; i<n; i++)
+        {
+            if(b[i]==a[j])
+                j++;
+            else
+            {
+                if(!flag)
+                    flag=true;
+                else
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+   
+    static bool myComp(string& a, string& b)
+    {
+        return a.size()<b.size();
+    }
+    
+    int longestStrChain(vector<string>& words) 
+    {
+        int n=words.size();
+        vector<int> t(n, 1);
+        int ans=1;
+        
+        sort(words.begin(), words.end(), myComp);
+        
+        for(int i=1; i<n; i++)
+        {
+            for(int j=0; j<i; j++)
+            {
+                int m=words[i].size();
+                int n=words[j].size();
+                
+                if(n+1==m&&isValid(words[j], words[i])&&t[i]<t[j]+1) 
+                   t[i]=t[j]+1;
+                ans=max(ans, t[i]);
+            }
+        }
+        return ans;
+    }  
+    
+    /*
+    int lcs(string a, string b)
+    {
+        int m=a.size();
+        int n=b.size();
+        
+        vector<vector<int>> t(m+1, vector<int>(n+1, 0));
+        
+        for(int i=0; i<=m; i++)
+        {
+            for(int j=0; j<=n; j++)
+            {
+                if(i==0||j==0)
+                    t[i][j]=0;
+                else if(a[i-1]==b[j-1])
+                    t[i][j]=1+t[i-1][j-1];
+                else
+                    t[i][j]=max(t[i-1][j], t[i][j-1]);
+            }
+        }
+        
+        return t[m][n];
+    }
+    
+    bool static myComp(string& a, string& b)
+    {
+        return a.size()<b.size();
+    }
+    
+    int longestStrChain(vector<string>& words) 
+    {
+        int n=words.size();
+        vector<int> t(n, 1);
+        
+        sort(words.begin(), words.end(), myComp);
+        
+        for(int i=1; i<n; i++)
+        {
+            for(int j=0; j<i; j++)
+            {
+                int m=words[i].size();
+                int n=words[j].size();
+                
+                if(m-n==1&&lcs(words[i], words[j])==n&&t[i]<t[j]+1)
+                    t[i]=t[j]+1;
+            }
+        }
+        
+        return *max_element(t.begin(), t.end());
+    }  */
+};
