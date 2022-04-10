21a7c4ab8b84e13253decdaaf0af4b02522f87fb
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-04-10 09:59:28 +0530

    Time: 68 ms (21.28%), Space: 12.4 MB (57.74%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -8,14 +8,13 @@ public:
         
         if(i<0||i>=m||j<0||j>=n||grid[i][j]=='0')
             return;
+        
         grid[i][j]='0';
         
         dfs(grid, i+1, j);
         dfs(grid, i-1, j);
         dfs(grid, i, j+1);
         dfs(grid, i, j-1);
-        
-        return;
     }
     
     int numIslands(vector<vector<char>>& grid) 
@@ -24,12 +23,15 @@ public:
         for(int i=0; i<grid.size(); i++)
         {
             for(int j=0; j<grid[0].size(); j++)
+            {
                 if(grid[i][j]=='1')
                 {
                     dfs(grid, i, j);
                     ctr++;
                 }
-        }    
+            }
+        }
+        
         return ctr;
     }
 };
