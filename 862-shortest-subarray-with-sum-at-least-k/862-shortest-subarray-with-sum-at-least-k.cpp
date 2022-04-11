13317e6bfba15704d6ddfd61afac39b2151fb5a1
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-04-11 17:59:59 +0530

    Time: 224 ms (94.68%), Space: 117.9 MB (19.28%) - LeetHub

--- 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.cpp
+++ 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    
     int shortestSubarray(vector<int>& nums, int k)
     {
         int n=nums.size();
@@ -12,19 +11,18 @@ public:
 
         deque<pair<long long int,int>>dq;
         dq.push_back({0,-1}); 
-        // This is because in the starting, the deque is empty so if we 
-        //didn't include this may miss out on things when the nums[0]       itself is >=k
+       
 
         int res=INT_MAX; 
         for(int i=0;i<n;i++)
         {
             long long int val=pre[i];
-            while(!dq.empty()&&dq[dq.size()-1].first>=val)
+            while(!dq.empty()&&dq.back().first>=val)
                 dq.pop_back();
             
             dq.push_back({pre[i],i});
             
-            while(!dq.empty()&&val-dq[0].first>=k)
+            while(!dq.empty()&&val-dq.front().first>=k)
             {
                 res=min(res, i-dq[0].second);
                 dq.pop_front();
