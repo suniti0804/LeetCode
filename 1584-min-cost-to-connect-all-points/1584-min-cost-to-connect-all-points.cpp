023f3fcadca96b4255815cb3aae85229f6e0fb36
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-04-13 19:01:23 +0530

    Time: 1007 ms (32.08%), Space: 177.7 MB (9.14%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
     
-    int find_set(vector<int>& parent, int i)
+    //Kruskal's
+   /* int find_set(vector<int>& parent, int i)
     {
-        if(parent[i]<0)
+        if(parent[i]==-1) 
             return i;
-       return parent[i] = find_set(parent, parent[i]);	//optimisation 
+       return parent[i]=find_set(parent, parent[i]);	//optimisation 
+    }
+    
+    void union_sets(int a, int b, vector<int>& sz, vector<int>& parent)
+    {
+        a=find_set(parent, a);
+        b=find_set(parent, b);
+
+        if(a!=b)
+          {
+              if(sz[a]<sz[b])
+                swap(a, b);
+              parent[b]=a;
+              sz[a]+=sz[b];	
+          }
     }
     
     int minCostConnectPoints(vector<vector<int>>& points) 
@@ -13,6 +28,7 @@ public:
         int n=points.size();
         int res=0;
         vector<int> parent(n, -1);
+        vector<int> sz(n, 1);
         priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
 
         for(int i=0; i<n; i++)
@@ -33,20 +49,54 @@ public:
             int x=p[1];
             int y=p[2];
             
-            x=find_set(parent, x);
-            y=find_set(parent, y);
+            int u=find_set(parent, x);
+            int v=find_set(parent, y);
             
-            if(x!=y)
+            if(u!=v)
             {
                 res+=w;
-                parent[x]+=parent[y];
-                parent[y]=x;
-                if(parent[x]==-n)
-                    break;
+                union_sets(x, y, sz, parent);
             }
         }
         
         return res;
         
+    }  */
+    
+    //Prim's
+    
+    int minCostConnectPoints(vector<vector<int>>& points) 
+    {
+        int n=points.size();
+		
+		set<pair<int, int>> vis,unvis;  //visited and non-visited sets are used to consider the points int the MST and remaining
+        for(int i=0;i<n;i++) 
+            unvis.insert({points[i][0],points[i][1]});  //All points in un-visited
+
+		//Priority queue is having distance as key(from growing MST) and pair<int,int> for coordinates in graph
+         priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+
+        int ans=0; 
+        pq.push({0, points[0][0], points[0][1]});  
+        
+        while(!pq.empty()&&vis.size()<n)
+        {
+            auto curr=pq.top(); 
+            pq.pop();  //minimum distance coordinate/vertex from growing ST.
+            if(vis.find({curr[1], curr[2]})!=vis.end()) 
+                continue; 
+            vis.insert({curr[1], curr[2]}); 
+            unvis.erase({curr[1], curr[2]}); //remove from unvis and insert into vis (like boolean marking)
+
+			for(auto itr:unvis)   // now consider only those vertices which have not been visited before
+            {
+                int dist=abs(curr[1]-itr.first)+abs(curr[2]-itr.second);
+                pq.push({dist,itr.first,itr.second});
+            }
+			
+            ans+=curr[0];
+        }
+        return ans;
     }
+    
 };
