6bea7666057ffd8f09b43d22a8f27e08e69d30a3
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-04-18 06:21:00 +0530

    Time: 33 ms (45.73%), Space: 20.7 MB (16.14%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) 
+    bool isValidSudoku(vector<vector<char>>& matrix) 
     {
         unordered_map<string, int> row, col, box;
-        
         for(int i=0; i<9; i++)
         {
             for(int j=0; j<9; j++)
             {
-                if(board[i][j]!='.')
+                if(matrix[i][j]!='.')
                 {
-                    char val=board[i][j];
-                    string r=to_string(i)+val;
-                    string c=to_string(j)+val;
-                    string b=to_string(i/3)+to_string(j/3)+val;
-                    
+                    int num=matrix[i][j]-'0';
+                    string r=to_string(num)+to_string(i);
+                    string c=to_string(num)+to_string(j);
+                    string b=to_string(num)+to_string(i/3)+to_string(j/3);
+
                     if(row[r]++>0||col[c]++>0||box[b]++>0)
                         return false;
                 }
+               
             }
         }
-        
+
         return true;
         
     }
