1334e6080a006311c8b45f24b36bff70cc3b78b2
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-05-04 18:50:04 +0530

    Time: 125 ms (31.10%), Space: 26.8 MB (98.78%) - LeetHub

---
+++ 478-generate-random-point-in-a-circle/478-generate-random-point-in-a-circle.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    double r, x, y;
+    
+    Solution(double radius, double x_center, double y_center) 
+    {
+        r=radius;
+        x=x_center;
+        y=y_center;
+    }
+    
+    bool isInside(double X, double Y)
+    {
+        return ((X-x)*(X-x)+(Y-y)*(Y-y))<=r*r;
+    }
+    
+    vector<double> randPoint() 
+    {
+        double X, Y;
+        while(!isInside(X, Y))
+        {
+            X=(double)rand()/RAND_MAX*(r+r)+x-r;
+            Y=(double)rand()/RAND_MAX*(r+r)+y-r;
+        }
+        return {X,Y}; 
+    }
+    
+    
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(radius, x_center, y_center);
+ * vector<double> param_1 = obj->randPoint();
+ */
