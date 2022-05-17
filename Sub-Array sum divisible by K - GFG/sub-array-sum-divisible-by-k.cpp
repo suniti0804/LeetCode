b0150f140ec800c1bb97809f6363ed65346df688
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-05-17 19:59:39 +0530

    Added solution - LeetHub

---
+++ Sub-Array sum divisible by K - GFG/sub-array-sum-divisible-by-k.cpp
@@ -0,0 +1,57 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+
+	public:
+	long long subCount(long long arr[], int N, long long k)
+	{
+	    long long ctr=0;
+	    vector<int> mod(k, 0);
+	    mod[0]=1;
+	    long long sum=0;
+	    
+	    for(int i=0; i<N; i++)
+	    {
+	        sum+=arr[i];
+	        long long rem=((sum%k)+k)%k;
+	        if(mod[rem]!=0)
+	            ctr+=mod[rem];
+	        mod[rem]++;
+	    }
+	   
+	   return ctr;
+	}
+
+
+
+};
+
+
+// { Driver Code Starts.
+
+int main() 
+{
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n;
+       	long long k;
+		cin>>n >> k;
+		long long a[n];
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+        
+
+        Solution ob;
+        cout << ob.subCount(a, n, k);
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+  // } Driver Code Ends
