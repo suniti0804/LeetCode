400e2782dbea70e542cb444b110259206387f264
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-05-21 12:28:08 +0530

    Time: 127 ms (44.02%), Space: 14.4 MB (54.73%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
+    //unbounded knapsack
     int coinChange(vector<int>& coins, int n) 
     {
         vector<int> t(n+1, INT_MAX-1);
         
-        t[0] = 0;
-        for (int i = 1; i <= n; i++) 
+        t[0]=0;
+        for (int i=1; i<=n; i++) 
         {
-            for (int j = 0; j < coins.size(); j++) 
+            for (int j=0; j<coins.size(); j++) 
             {
-                if (coins[j] <= i) 
-                  t[i] = min(t[i], t[i - coins[j]] + 1);
+                if(coins[j]<=i) 
+                  t[i]=min(t[i], t[i-coins[j]]+1);
             }
         }
-        return t[n] == INT_MAX - 1 ? -1 : t[n];
-        
+        return t[n]==INT_MAX-1?-1:t[n];    
     }
 };
