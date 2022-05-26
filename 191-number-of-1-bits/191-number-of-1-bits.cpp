9d8a461c9b8a43292d16325d3e7833fce26e3a5d
Suniti <72435330+suniti0804@users.noreply.github.com>
2022-05-26 17:01:44 +0530

    Time: 0 ms (100.00%), Space: 6 MB (48.74%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    
+    /*return __builtin_popcount(n);
+        //Similarly you can use __builtin_popcountl(x) &          __builtin_popcountll(x) for long and long long data types.*/
     int hammingWeight(uint32_t n) 
     {
         int res=0;
