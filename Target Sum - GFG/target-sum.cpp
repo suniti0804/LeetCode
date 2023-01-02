f988db9b9abc46c6de8910436286b27be34a5ae7
Suniti <72435330+suniti0804@users.noreply.github.com>
2023-01-02 20:59:19 +0530

    Added solution - LeetHub

---
+++ Target Sum - GFG/target-sum.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//Back-end complete function Template for C++
+
+class Solution {
+  public:
+  
+   int countSubsets(vector<int>& nums, int n, int M)
+    {
+        int t[n+1][M+1];
+
+        for(int i=0; i<=n; i++)
+        {
+            for(int j=0; j<=M; j++)
+            {
+                if(i==0)
+                    t[i][j]=0;
+                if(j==0)
+                    t[i][j]=1;
+            }
+        }
+
+        //t[0][0] = 1;
+
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=0; j<=M; j++)
+            {
+                if(nums[i-1]<=j)
+                    t[i][j]=t[i-1][j-nums[i-1]]+t[i-1][j];
+                else
+                    t[i][j]=t[i-1][j];
+            }
+        }
+
+        return t[n][M];  
+    }
+  
+    int findTargetSumWays(vector<int> &nums ,int target) {
+        target=abs(target);
+         int n=nums.size();
+         int sum=0;
+
+         for(int i=0; i<n; i++)
+             sum+=nums[i];
+
+        int M=(sum+target)/2;
+        if(sum<target||(sum+target)%2!=0)
+            return 0;
+        
+         return countSubsets(nums, n, M);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        vector<int>arr(N);
+        
+        for(int i=0 ; i<N ; i++){
+            cin>>arr[i];
+        }
+        int target;
+        cin >> target;
+
+        Solution ob;
+        cout<<ob.findTargetSumWays(arr,target);
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
